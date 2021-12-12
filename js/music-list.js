e814525460090f00a45dc525c3f075ab9760664c
Doniws <doniwahsap@gmail.com>
2021-12-12 18:52:30 +0700

    New

---
+++ js/music-list.js
@@ -0,0 +1,141 @@
+
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .webp formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+let allMusic = [
+  {
+    name: " #1",
+    artist: "Imagine Dragons",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Whatever it take",
+    artist: "Imagine Dragons",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Start Again ft. Logic",
+    artist: "OneRepublic",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: " Spirits ",
+    artist: "The Strumbellas",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Let Her Go",
+    artist: "Passenger",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "(New Music)- Monday",
+    artist: "Imagine Dragons",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Let You Down",
+    artist: "NF",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Stressed Out ",
+    artist: "twenty one pilots",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Let's Hurt Tonight ",
+    artist: "OneRepublic",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Yellow ",
+    artist: "Coldplay",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Chlorine ",
+    artist: "twenty one pilots",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "CASTLE OF GLASS ",
+    artist: "Linkin Park ",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: " Demons ",
+    artist: "Imagine Dragons",
+    img: "music-13",
+    src: "music-13"
+  },
+  {
+    name: "When I Grow Up",
+    artist: "NF",
+    img: "music-14",
+    src: "music-14"
+  },
+  {
+    name: "Leave Me Alone ",
+    artist: "NF",
+    img: "music-15",
+    src: "music-15"
+  },
+
+  {
+    name: "My Life ",
+    artist: "Imagine Dragons",
+    img: "music-16",
+    src: "music-16"
+  },
+  {
+    name: "Morph ",
+    artist: "twenty one pilots",
+    img: "music-17",
+    src: "music-17"
+  },
+  {
+    name: "Message Man ",
+    artist: "twenty one pilots",
+    img: "music-18",
+    src: "music-18"
+  },
+  {
+    name: " Fairly Local ",
+    artist: "twenty one pilots",
+    img: "music-19",
+    src: "music-19"
+  },
+  {
+    name: "The Judge ",
+    artist: "twenty one pilots",
+    img: "music-20",
+    src: "music-20"
+  },
+
+  
+
+  
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .webp formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
