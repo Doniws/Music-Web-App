63523721a90f077fdfed39bcba4a5cb12cf934ed
Doniws <doniwahsap@gmail.com>
2022-06-13 20:03:17 +0700

    Cleaning File

--- js/all-script.js
+++
@@ -1,7 +0,0 @@
-// PRELOADER
-// var loader = document.getElementById("preloader")
-
-// window.addEventListener("load", function() {
-//     loader.style.display = "none";
-// })
-// const swup = new Swup(); // only this line when included with script tag
